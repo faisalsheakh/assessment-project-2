89b7b09ae8252decd24bad0920a542cadbbab366
Faisal Asad <faisal.18jics123@jietjodhpur.ac.in>
2020-06-07 20:34:17 +0530

    menue added

--- script/main.js
+++ script/main.js
@@ -7,22 +7,24 @@
     xhr.onload = function(event) {
         var data = JSON.parse(this.responseText)
         var output = '';
+        var headding = '';
         if (this.status == 200)
-        {
+        {   headding +='<table><th>State</th><th>Total Confirmed</th><th>Active</th><th>Recovered</th><th>Deaths</th><th>Today New Cases</th><th>Recovered Today</th><th>Deaths Today</th></table>'
+            document.getElementById('haeding').innerHTML= headding;
             for(var i in this.responseText)
             {
             console.log(data.statewise[i].state)
 
-             output +='<div class="states"><ul>'
+             output +='<div class="states"><ul><table><th>'
              + data.statewise[i].state+
-             '<li class="total">Total confirmed :-' + data.statewise[i].confirmed +
-             '<li class="active">Active :-' + data.statewise[i].active +
-             '<li class="recv">Total Recovered :-' + data.statewise[i].recovered +
-             '<li class="deaths">Total Deaths :-' + data.statewise[i].deaths +
-             '<li class="conf">Today New Cases:-' + data.statewise[i].deltaconfirmed +
-             '<li class="recv">Recovered Today :-' + data.statewise[i].deltarecovered +
-             '<li class="deaths">Deaths Today :-' + data.statewise[i].deltadeaths +
-             '</div></br>';
+             '</th><td><li class="total">'+ data.statewise[i].confirmed +
+             '</td><td><li class="active">'+ data.statewise[i].active +
+             '</td><td><li class="recv">'+ data.statewise[i].recovered +
+             '</td><td><li class="deaths">'+ data.statewise[i].deaths +
+             '</td><td><li class="conf">'+ data.statewise[i].deltaconfirmed +
+             '</td><td><li class="recv">'+ data.statewise[i].deltarecovered +
+             '</td><td><li class="deaths">'+ data.statewise[i].deltadeaths +
+             '</table></div></br>';
              document.getElementById('cont').innerHTML= output;
             }
             
